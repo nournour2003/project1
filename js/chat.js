b2830aff7625060bdf6b3c5b2f881133f3d88e31
nournour2003 <nouralnour57@gmail.com>
2025-01-12 22:14:08 +0300

    Add files via upload

---
+++ js/chat.js
@@ -0,0 +1,1 @@
+window.$crisp=[];window.CRISP_WEBSITE_ID="cda2828d-69c3-4a06-9716-6767d0e60754";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();
